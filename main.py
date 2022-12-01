19f5de527bb5564c3131545936720a88f757bd63
Mateo <miniatimat@gmail.com>
2022-12-01 11:35:34 -0300

    Create promo almost woeking (Missing click create button)
    Started work in assignPromo

--- main.py
+++ main.py
@@ -57,17 +57,29 @@ def create_promo(promoCode, games, selectedCurrency):
     #Input promo code
     WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00-create-promotion_code"))).send_keys(promoCode)
 
+    #Select Mode (By default is Fun)
+    modesField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00_create_game_mode_chosen")))
+    modeSelector = modesField.find_element(By.CSS_SELECTOR, "a")
+    modeSelector.click()
+    modes = modesField.find_elements(By.CSS_SELECTOR, "li")
+    modes[0].click()
+
     #input number of plays (By default is 10)
     WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00-create-plays_total"))).send_keys(10)
 
     #Input bet ammount (By default is 100)
     WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00-create-total_bet"))).send_keys(100)
 
+    #Input currency, (By default is USD)
+    currencyField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00_create_currency_id_chosen")))
+    currencySelector = currencyField.find_element(By.CSS_SELECTOR, "a")
+    currencySelector.click()
+    currencies = currencyField.find_elements(By.CSS_SELECTOR, "li")
+    currencies[116].click() #USD
+
     #Input start date and expiration date
     WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00-create-start_date"))).send_keys(date.today().strftime("%y-%m-%d "))
     WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00-create-expiration_date"))).send_keys((date.today()+datetime.timedelta(days=1)).strftime("%y-%m-%d "))
-
-
 
     #Input allowed operators
     allowedOperatorsField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00_create_operator_id_chosen")))
@@ -94,23 +106,39 @@ def create_promo(promoCode, games, selectedCurrency):
     allowedCurrency.send_keys(selectedCurrency)
     allowedCurrency.send_keys(Keys.ENTER)
 
-    #Select Mode (By default is Fun)
-    modesField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00_create_game_mode_chosen")))
-    modeSelector = modesField.find_element(By.CSS_SELECTOR, "a")
-    modeSelector.click()
-    modes = modesField.find_elements(By.CSS_SELECTOR, "li")
-    modes[0].click()
+    DRIVER.find_element(By.ID, "CMP00-create").submit()
+    time.sleep(5)
+    DRIVER.quit()
 
-    currencyField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CMP00_create_currency_id_chosen")))
-    currencySelector = currencyField.find_element(By.CSS_SELECTOR, "a")
-    currencySelector.click()
-    currency = modesField.find_element(By.CSS_SELECTOR, "input")
+def assignPromo(promoCode, users):
+    DRIVER.get("https://tools.rubicon.vsslots.com/bob/login.php?r=https://tools.rubicon.vsslots.com/bob/applications/QA/WTR00/")
+    WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("mateo.miniati")
+    WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("09Sep1998")
+    WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.NAME, "submit"))).click()
 
+    #Go to assign Promotion
+    DRIVER.get("https://tools.rubicon.vsslots.com/bob/applications/FRP/CIT00/#create")
+    time.sleep(1)
+
+    promoIDField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CIT00_create_promotion_definition_id_chosen")))
+    promoID = promoIDField.find_element(By.CSS_SELECTOR, "a")
+    promoID.click()
+    textbox = promoIDField.find_element(By.CSS_SELECTOR, "input")
+    textbox.send_keys(promoCode)
+    textbox.send_keys(Keys.ENTER)
+
+    userField = WebDriverWait(DRIVER, 10).until(EC.presence_of_element_located((By.ID, "CIT00-create-user_identifier")))
+    userField.send_keys(users[0])
+    for user in users:
+        userField.send_keys(user)
+        userField.send_keys(Keys.ENTER)
 
 
     time.sleep(5)
     DRIVER.quit()
 
-create_promo("promoTest", ["chocolate", "salariazo"], "US Dollar")
+
+assignPromo("MateoCodex", ["mateo@VIBRA"])
+#create_promo("promoTest", ["chocolate", "salariazo"], "US Dollar")
 
 #Hablar con Diego Zamora o Fabio Quintana
